39948ffb68238793c13c00b50e16789692840b60
graySquirrel <fritzebner@gmail.com>
2015-11-14 11:32:34 -0500

    sample file for how to work with Mendeley

--- mendeley.R
+++ mendeley.R
@@ -4,22 +4,31 @@ library(RCurl)
 library(yaml)
 library(RColorBrewer)
 
-config <- yaml.load_file('config.yml.example')
+# For this to authenticate properly, you must have your default browser open.
+# A page will come up and ask you to authorize if the token has expired.
 
-use_proxy("proxy.useastgw.xerox.com", port = 8000)
+#use_proxy("proxy.useastgw.xerox.com", port = 8000)
 
 # Note: the redirect URI in Mendeley *has* to be http://localhost:1410/
 mendeley <- oauth_endpoint(base_url = 'https://api.mendeley.com/oauth', 
                            authorize = 'authorize', access = 'token')
-myapp <- oauth_app(appname = 'funWithSOTL', key = "2363", secret = "dmI2oHj00OpHcqLY")
+myapp <- oauth_app(appname = 'funWithSOTL', key = "2363", secret = "j3zBZPNaHRMMtox1")
 token <- oauth2.0_token(mendeley, myapp, scope='all',cache=TRUE)
 
-profile_rsp <- GET('https://api.mendeley.com/profiles/me', 
-                   config(token = token),
-                   c(use_proxy("proxy.useastgw.xerox.com", port = 8000), verbose()))
+profile_rsp <- GET('https://api.mendeley.com/profiles/me', config(token = token))
 
 profile <- fromJSON(rawToChar(content(profile_rsp)))
 message(paste('Hello, ', profile$display_name, '!', sep=''))
+
+#### NOW I am logged in.  go get data.
+groups <- GET('https://api.mendeley.com/groups', config(token = token))
+groups_content <- fromJSON(rawToChar(content(groups)))
+
+documents <- GET(paste0('https://api.mendeley.com/documents?group_id=',
+                        groups_content[[1]]$id), config(token = token))
+documents_content <- fromJSON(rawToChar(content(documents)))
+
+
 
 doi <- readline('Enter a DOI: ')
 doc_rsp <- GET(paste('https://api.mendeley.com/catalog?view=stats&doi=', curlEscape(doi), sep=''), config(token = token))
