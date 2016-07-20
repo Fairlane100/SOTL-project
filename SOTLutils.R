2a30247b96114129c7d9158c0cd34e097055798a
graySquirrel <fritzebner@gmail.com>
2016-07-20 11:13:46 -0400

    Revert "new files for preprocessing input. taking too long to do every time."
    This reverts commit 2cc57b1fa30f9e39d441625f4ec454a17cf7035c.

--- SOTLutils.R
+++ SOTLutils.R
@@ -12,7 +12,10 @@ cleanText <- function(txtVec, stops, equivwords, concatTerms) {
     concatTerms <- tolower(concatTerms)
     # remove URLs
     tmpVec <- gsub('(f|ht)tp\\S+\\s*', "", tmpVec)
-
+    # remove funky chars that gsub doesnt seem to be able to deal with
+    old1 <- "äó_„ŽñîÑñîñ"
+    new1 <- paste(rep(" ",nchar(old1)),collapse = "")
+    tmpVec <- chartr(old1,new1,tmpVec)
     # remove punctuation
     tmpVec <- gsub("[^[:alnum:][:space:]]", " ", tmpVec)
     # remove numbers
@@ -41,7 +44,8 @@ cleanText <- function(txtVec, stops, equivwords, concatTerms) {
         #print(paste("repl",patt,"with",""))
         tmpVec <-gsub(patt, cpat, tmpVec)
     }
-    
+    # remove short words
+    tmpVec <- rm_nchar_words(tmpVec, "1,2")
     # Remove numbers
     tmpVec <- removeNumbers(tmpVec)
     # Remove punct
