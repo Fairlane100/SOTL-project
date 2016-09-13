c7e7f87f07205c2ab3e6e9a43ebe6d12bc71163c
graySquirrel <fritzebner@gmail.com>
2016-09-13 14:34:40 -0400

    updated

--- scripts/findWordsInContext.R
+++ scripts/findWordsInContext.R
@@ -37,5 +37,5 @@ for (i in 1:length(c$abstract)) {
 }
 if (!is.null(resultDf)){
     colnames(resultDf) <- c("Abstract","Disc","Match")
-    write.csv(resultDf,paste0("../working/wordsInContextResult_Word_",THEWORD,"_Context_",N,"_Discipline_",DISC,".csv"))
+    write.csv(resultDf,paste0("wordsInContextResult_Word_",THEWORD,"_Context_",N,"_Discipline_",DISC,".csv"))
 }
