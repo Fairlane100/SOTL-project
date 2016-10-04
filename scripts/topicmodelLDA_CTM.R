97629dd076fc639220c358e05528c64b25a94c47
graySquirrel <fritzebner@gmail.com>
2016-10-04 09:08:33 -0400

    enable Mallet LDAvis

--- scripts/topicmodelLDA_CTM.R
+++ scripts/topicmodelLDA_CTM.R
@@ -30,6 +30,8 @@ summary(term_tfidf)
 # 0.03408 0.17690 0.22270 0.27290 0.30040 2.34900 
 
 dtm <- dtm[,term_tfidf >= 0.23]
+df <- orig[row_sums(dtm) > 0,] # for later mallet
+
 dtm <- dtm[row_sums(dtm) > 0,]
 summary(col_sums(dtm))
 # Min.  1st Qu.   Median     Mean  3rd Qu.     Max. 
@@ -84,6 +86,35 @@ dtm2list <- apply(dtm, 1, function(x) {
 
 ## convert to a Corpus
 myCorp <- VCorpus(VectorSource(dtm2list))
+#http://rstatistics.net/text-mining-with-lda/
+# do a Mallet LDA also to compare all together.
+# TODO: create a Mallet LDA
+# add names to corpus from dtm. Not getting into corpus for some reason.
+# Convert the model to LDAVis format using above link
+# NB: there is a reference there also about finding optimal number of topics.
+df$abstract <- dtm2list
+topout <- trainTopicModel(df, k)
+# phi is topic.words as probabilities
+phi <- as.matrix(topout[[1]])
+# term.frequency is vector of word counts of vocab
+# This is a workaround for a bug in term.freq from mallet.
+# https://github.com/mimno/RMallet/issues/2
+term.frequency <- topout[[3]]
+term.frequency$NEWterm.freq <- colSums(phi)
+phiNorm <- phi/rowSums(phi)
+# theta is doc.topics as probabilities
+theta <- as.matrix(topout[[2]])
+theta <- theta/rowSums(theta)
+# doc.length is num terms in each doc
+doc.length <- sapply(dtm2list,function(x) length(unlist(strsplit(x," "))))
+# vocab is a vector of terms
+vocab <- colnames(phi)
+
+json_lda_mallet <- LDAvis::createJSON(phi = phiNorm, theta = theta,
+                               vocab = vocab,
+                               doc.length = doc.length,
+                               term.frequency = term.frequency$NEWterm.freq)
+
 #inspect(myCorp)
 library(servr)
 json_VEM <- topicmodels_json_ldavis(fitted = TM[["VEM"]], corpus = myCorp, doc_term = dtm)
@@ -94,10 +125,28 @@ write(json_VEM, "../scripts/vis_app/json_VEM")
 write(json_VEMf, "../scripts/vis_app/json_VEMf")
 write(json_Gib, "../scripts/vis_app/json_Gib")
 write(json_CTM, "../scripts/vis_app/json_CTM")
+write(json_lda_mallet, "../scripts/vis_app/json_lda_mallet")
 
+#serVis(json_lda_mallet)
 #serVis(json_VEM)
 #serVis(json_VEMf)
 #serVis(json_Gib)
 #serVis(json_CTM)
 
+#### Get top topics for each doc from CTM, and line up the discipline for each doc
+# then connect them and visualize in a graph
+# just get the most likely topic for each document
+TopicsCTM <- topics(TM[["CTM"]],1)
+docinx <- names(TopicsCTM)
+Discs <- orig$Disc[as.numeric(docinx)]
+discnodes <- paste0("D",sort(unique(Discs)))
+topicnodes <- paste0("T",sort(unique(TopicsCTM)))
+allnodes <- c(discnodes,topicnodes)
+allnodeswithids <- data.frame(c(1:length(allnodes)),allnodes)
+colnames(allnodeswithids) <- c("id","label")
+alledges <- data.frame(source=Discs,target=TopicsCTM+length(discnodes))
 
+library(rgexf)
+path <- paste0(getwd(),"/gephi_CTM_Disc_Topic.gexf")
+write.gexf(allnodeswithids,alledges,output=path)
+gexfFile <- write.gexf(allnodeswithids,alledges)
