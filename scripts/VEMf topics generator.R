e42a791f1cca6114f9526fb03649ca94801cc834
graySquirrel <fritzebner@gmail.com>
2016-11-05 21:49:31 -0400

    printed out term freq before and after tfidf, made term freq for each topic and printed it

--- scripts/VEMf topics generator.R
+++ scripts/VEMf topics generator.R
@@ -11,8 +11,13 @@ a <- orig$abstract
 corpus <- Corpus(VectorSource(a))
 
 dtm <- DocumentTermMatrix(corpus)
-dtm$dimnames$Docs <- as.character(c(1:(dtm$nrow)))
+#dtm$dimnames$Docs <- as.character(c(1:(dtm$nrow)))
+dtm$dimnames$Docs <- orig$title
 dim(dtm)
+freq <- colSums(as.matrix(dtm))
+ord <- order(freq,decreasing=TRUE)
+freq[ord]
+write.csv(freq[ord],"word_freq_beforetfidf.csv")
 
 summary(col_sums(dtm))
 # Min.  1st Qu.   Median     Mean  3rd Qu.     Max. 
@@ -25,8 +30,12 @@ summary(term_tfidf)
 # 0.03408 0.17690 0.22270 0.27290 0.30040 2.34900 
 
 dtm <- dtm[,term_tfidf >= 0.23]
-df <- orig[row_sums(dtm) > 0,] # for later mallet
-
+freq <- colSums(as.matrix(dtm))
+ord <- order(freq,decreasing=TRUE)
+freq[ord]
+write.csv(freq[ord],"word_freq_aftertfidf.csv")
+# save a copy of orig with empty abstracts removed...
+orig0removed <- orig[row_sums(dtm) > 0,]
 dtm <- dtm[row_sums(dtm) > 0,]
 summary(col_sums(dtm))
 # Min.  1st Qu.   Median     Mean  3rd Qu.     Max. 
@@ -45,7 +54,26 @@ TM@alpha
 #Terms <- as.data.frame(terms(TM[["VEM_fixed"]], 30))
 Terms <- as.data.frame(terms(TM, 30))
 Terms[,1:k]
-write.csv(Terms,"../working/topicterms.csv", k)
+write.csv(Terms,paste("../working/topicterms ", k,".csv"))
 # trying to write the file in the working directory
 #write(json_VEMf, "../scripts/vis_app/json_VEMf")
                       
+### How to find term frequency of terms in specific topics
+# 1) assign each document to its most likely topic
+# 2) add up all term freqs for terms in each doc of its likely topic
+TMtopics <- topics(TM)
+#length(TMtopics)
+tfdf <- data.frame(1:30)
+for (t in 1:k) {
+    # sum term freqs for docs that belong to the topic
+    dtmt <- dtm[which(TMtopics==t),]
+    myTopicTermFreq <- col_sums(dtmt)
+    mySorted <- sort(myTopicTermFreq,decreasing = T)
+    topic <- names(mySorted[1:30])
+    freq <- as.vector(mySorted[1:30])
+    tfdf <- cbind(tfdf,topic)
+    tfdf <- cbind(tfdf, freq)
+}
+tfdf <- tfdf[,-1]
+colnames(tfdf) <- rep(1:k,each=2)
+write.csv(tfdf,paste("../working/topicsTermFreqs ", k,".csv"))
